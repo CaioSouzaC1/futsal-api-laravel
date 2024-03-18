fbb607b0ddd2e908ec1f6f5e6746b6192beb23c0
CaioSouzaC1 <caiocsouza2010@gmail.com>
2024-03-18 20:40:29 -0300

    <FIX>[Exception]: Fixing ApiException

--- app/Exceptions/ApiException.php
+++ app/Exceptions/ApiException.php
@@ -3,8 +3,9 @@
 namespace App\Exceptions;
 
 use App\Builder\ReturnApi;
+use Exception;
 
-class ApiException
+class ApiException extends Exception
 {
 
     public function __construct(String $error = "Erro inesperado")
